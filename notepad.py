bb7dd1ba008383aeff9016b2960f1d777ee96bfd
FernandoRD <frdurso@gmail.com>
2023-06-25 22:07:01 -0300

    update ts_check_v2 and notepad.py

--- notepad.py
+++ notepad.py
@@ -9,42 +9,63 @@ import sys
 from pyvirtualdisplay import Display
 import Xlib.display
 import argparse
+import datetime
+
 
 def actions(pyautogui, resilienceValue, confidenceValue, debug, disp, interval, failure_search_buttom, failure_search):    
     i = 0
     while i < resilienceValue:
         try:
-            x,y = pyautogui.locateCenterOnScreen("./search_buttom.png", grayscale=True, confidence=confidenceValue)
-        except Exception as e:
-            failure_search_buttom += 1
-            if failure_search_buttom == resilienceValue:
-                print("CRITICAL: Could not find search buttom!")
+            pyautogui.hotkey('win', 's')
+            time.sleep(interval)
+            x,y = pyautogui.locateCenterOnScreen("./search.png", grayscale=True, confidence=confidenceValue)
+            pyautogui.moveTo(x,y)
+            pyautogui.click()
+        except:
+            try:
+                x,y = pyautogui.locateCenterOnScreen("./search_buttom.png", grayscale=True, confidence=confidenceValue)
+            except Exception as e:
+                failure_search_buttom += 1
+                if failure_search_buttom == resilienceValue:
+                    print("CRITICAL: Could not find search buttom!")
+                    if debug:
+                        print(f"Search Buttom recognition failures: {failure_search_buttom}")
+                        pyautogui.screenshot('./screenshot_failure_search_buttom.png')
+                    disp.stop()
+                    sys.exit(2)
+                time.sleep(interval)
+                i += 1
+                print("Aqui falhou 1")
+            else:
+                pyautogui.moveTo(x,y)
+                time.sleep(interval)
+                pyautogui.click()
                 if debug:
                     print(f"Search Buttom recognition failures: {failure_search_buttom}")
                     pyautogui.screenshot('./screenshot_failure_search_buttom.png')
-                disp.stop()
-                sys.exit(2)
-            time.sleep(interval)
-            i += 1
-            print("Aqui falhou 1")
+                i += resilienceValue
+                print("Aqui achou 1")
         else:
-            pyautogui.moveTo(x,y)
+            #pyautogui.moveTo(x,y)
+            #time.sleep(interval)
+            pyautogui.typewrite('notepad ', interval=0.1)
             time.sleep(interval)
-            pyautogui.click()
             if debug:
-                print(f"Search Buttom recognition failures: {failure_search_buttom}")
-                pyautogui.screenshot('./screenshot_failure_search_buttom.png')
+                print(f"Search recognition failures: {failure_search}")
+                pyautogui.screenshot('./screenshot_failure_search.png')
+            pyautogui.press('enter')
+            time.sleep(interval)
             i += resilienceValue
-            print("Aqui achou 1")
+            print("Aqui achou 2")
 
     i = 0
     while i < resilienceValue:
         try:
-            x,y = pyautogui.locateCenterOnScreen("./search.png", grayscale=True, confidence=confidenceValue)                    
+            x,y = pyautogui.locateCenterOnScreen("./notepad.png", grayscale=True, confidence=confidenceValue)                    
         except Exception as e:
             failure_search += 1
             if failure_search == resilienceValue:
-                print("CRITICAL: Could not find search field!")
+                print("CRITICAL: Could not find notepad!")
                 if debug:
                     print(f"Search recognition failures: {failure_search}")
                     pyautogui.screenshot('./screenshot_failure_search.png')
@@ -52,14 +73,26 @@ def actions(pyautogui, resilienceValue, confidenceValue, debug, disp, interval, 
                 sys.exit(2)
             time.sleep(interval)
             i += 1
-            print("Aqui falhou 2")
+            print("Aqui falhou 3")
         else:
             pyautogui.moveTo(x,y)
             time.sleep(interval)
-            pyautogui.typewrite('notepad', interval=0.2)
+            pyautogui.click()
+            time.sleep(interval)
+            pyautogui.typewrite('Some test you wanna write', interval=0.1)
+            time.sleep(interval)
+            pyautogui.hotkey('ctrl', 's')
+            time.sleep(interval)
+            x = datetime.datetime.now()
+            pyautogui.typewrite(f'arquivo-{x.strftime("%Y-%m-%d %H-%M-%S")}.txt', interval=0.1)
+            time.sleep(interval)
+            pyautogui.press('enter')
+            time.sleep(interval)
+            pyautogui.hotkey('alt', 'f4')
+            time.sleep(interval)
             if debug:
                 print(f"Search recognition failures: {failure_search}")
                 pyautogui.screenshot('./screenshot_failure_search.png')
             pyautogui.press('enter')
             i += resilienceValue
-            print("Aqui achou 2")
+            print("Aqui achou 3")
--- ts_check_v2.py
+++ ts_check_v2.py
@@ -133,65 +133,71 @@ def main():
 
     ################################################################################################################
     # LOGOFF PROCESS BEGINS...
-    i = 0
-    while i < resilienceValue:
-        try:
-            x,y = pyautogui.locateCenterOnScreen("./search_buttom.png", grayscale=True, confidence=confidenceValue)
-        except Exception as e:
-            failure_search_buttom += 1
-            if failure_search_buttom == resilienceValue:
-                print("CRITICAL: Could not find search buttom!")
-                if debug:
-                    print(f"Search Buttom recognition failures: {failure_search_buttom}")
-                    pyautogui.screenshot('./screenshot_failure_search_buttom.png')
-                disp.stop()
-                sys.exit(2)
-            time.sleep(interval)
-            i += 1
-            print("Aqui falhou 1")
-        else:
-            pyautogui.moveTo(x,y)
-            time.sleep(interval)
-            pyautogui.click()
-            if debug:
-                print(f"Search Buttom recognition failures: {failure_search_buttom}")
-                pyautogui.screenshot('./screenshot_failure_search_buttom.png')
-            i += resilienceValue
-            print("Aqui achou 1")
+    pyautogui.hotkey('win', 's')
+    time.sleep(interval)
+    pyautogui.typewrite('logoff', interval=0.1)
+    time.sleep(interval)
+    pyautogui.press('enter')
+#    i = 0
+#    while i < resilienceValue:
+#        try:
+#            x,y = pyautogui.locateCenterOnScreen("./search_buttom.png", grayscale=True, confidence=confidenceValue)
+#        except Exception as e:
+#            failure_search_buttom += 1
+#            if failure_search_buttom == resilienceValue:
+#                print("CRITICAL: Could not find search buttom!")
+#                if debug:
+#                    print(f"Search Buttom recognition failures: {failure_search_buttom}")
+#                    pyautogui.screenshot('./screenshot_failure_search_buttom.png')
+#                disp.stop()
+#                sys.exit(2)
+#            time.sleep(interval)
+#            i += 1
+#            print("Aqui falhou 1")
+#        else:
+#            pyautogui.moveTo(x,y)
+#            time.sleep(interval)
+#            pyautogui.click()
+#            if debug:
+#                print(f"Search Buttom recognition failures: {failure_search_buttom}")
+#                pyautogui.screenshot('./screenshot_failure_search_buttom.png')
+#            i += resilienceValue
+#            print("Aqui achou 1")
     
-    i = 0
-    while i < resilienceValue:
-        try:
-            x,y = pyautogui.locateCenterOnScreen("./search.png", grayscale=True, confidence=confidenceValue)                    
-        except Exception as e:
-            failure_search += 1
-            if failure_search == resilienceValue:
-                print("CRITICAL: Could not find search field!")
-                if debug:
-                    print(f"Search recognition failures: {failure_search}")
-                    pyautogui.screenshot('./screenshot_failure_search.png')
-                disp.stop()
-                sys.exit(2)
-            time.sleep(interval)
-            i += 1
-            print("Aqui falhou 2")
-        else:
-            pyautogui.moveTo(x,y)
-            time.sleep(interval)
-            pyautogui.typewrite('logoff', interval=0.2)
-            if debug:
-                print(f"Search recognition failures: {failure_search}")
-                pyautogui.screenshot('./screenshot_failure_search.png')
-            pyautogui.press('enter')
-            total_time=time.time() - start_time
-            i += resilienceValue
-            print("Aqui achou 2")
-
+#    i = 0
+#    while i < resilienceValue:
+#        try:
+#            x,y = pyautogui.locateCenterOnScreen("./search.png", grayscale=True, confidence=confidenceValue)                    
+#        except Exception as e:
+#            failure_search += 1
+#            if failure_search == resilienceValue:
+#                print("CRITICAL: Could not find search field!")
+#                if debug:
+#                    print(f"Search recognition failures: {failure_search}")
+#                    pyautogui.screenshot('./screenshot_failure_search.png')
+#                disp.stop()
+#                sys.exit(2)
+#            time.sleep(interval)
+#            i += 1
+#            print("Aqui falhou 2")
+#        else:
+#            pyautogui.moveTo(x,y)
+#            time.sleep(interval)
+#            pyautogui.typewrite('logoff', interval=0.2)
+#            if debug:
+#                print(f"Search recognition failures: {failure_search}")
+#                pyautogui.screenshot('./screenshot_failure_search.png')
+#            pyautogui.press('enter')
+#            total_time=time.time() - start_time
+#            i += resilienceValue
+#            print("Aqui achou 2")
+#
     if debug:
         print(f"Statistics (Reconition failures): Load Screen: {failure_load_screen}, Search Buttom: {failure_search_buttom}, Search: {failure_search}")
     # Stop display after a successfull execution.     
     disp.stop()
-
+    
+    total_time=time.time() - start_time
     try:
         if total_time < warning:
             print(f"OK: Execution time: {total_time:.2f} s | exec_time={total_time:.2f}s;{warning};{critical};0;{critical*2}")
